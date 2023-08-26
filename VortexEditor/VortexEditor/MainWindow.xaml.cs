adf7e05e49c206fa9e8fbe19d1277d20d87d99c2
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2023-08-26 22:31:56 +0100

    Open and Create New project Buttons behaviours implemented

--- VortexEditor/VortexEditor/MainWindow.xaml.cs
+++ VortexEditor/VortexEditor/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VortexEditor.Game_Project;
 
 namespace VortexEditor
 {
@@ -23,6 +24,26 @@ namespace VortexEditor
         public MainWindow()
         {
             InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+
+            OpenProjectBrowserDialog();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+           var projectBrowser = new ProjectBrowserDialog();
+            if(projectBrowser.ShowDialog() == false)
+            {
+                Application.Current.Shutdown();
+            }
+            else {
+
+            }
         }
     }
 }
